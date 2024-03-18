e7e079f481482a62d61a8dd17739aff6be6b82f4
MaryamHmayed <mariam.hmayed.88@gmail.com>
2024-03-18 19:28:48 +0200

    get average ratings of flights

---
+++ backend/avgRating.php
@@ -0,0 +1,30 @@
+<?php
+include('connection.php');
+
+
+
+$query = $mysqli->prepare("SELECT flight_id, AVG(rating) AS average_rating
+FROM reviews
+GROUP BY flight_id;");
+
+$query->execute();
+$result = $mysqli->query($sql);
+
+   
+if ($result) {
+        $average_ratings = array();
+        while ($row = $result->fetch_assoc()) {
+            $average_ratings[] = $row;
+        }
+
+        $response['status'] = 'success';
+        $response['average_ratings'] = $average_ratings;
+    } else {
+        
+        $response['status'] = 'error';
+        $response['message'] = 'Failed to fetch average ratings';
+    }
+
+
+
+echo json_encode($response);
