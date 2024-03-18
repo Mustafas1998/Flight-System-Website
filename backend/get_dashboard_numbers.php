04c88b405b61d386c6452d827da9c1a7e9c45399
AliAbdelAall <3laych@gmail.com>
2024-03-18 06:15:54 +0200

    added get_dashboard_numbers.php file

---
+++ backend/get_dashboard_numbers.php
@@ -0,0 +1,22 @@
+<?php
+include("connection.php");
+
+$bookings_query = $mysqli -> prepare("select count(*) from bookings");
+$flights_query = $mysqli -> prepare("select count(*) from flights");
+$users_query = $mysqli -> prepare("select count(*) from users");
+
+$response = [];
+
+function storeValueFromQuery ($key, $query, $response) {
+  $query -> execute();
+  $query -> store_result();
+  $query -> bind_result($result);
+  $query -> fetch();
+  $response[$key] = $result;
+}
+
+storeValueFromQuery ("total_bookings", $bookings_query, $response);
+storeValueFromQuery ("total_flights", $flights_query, $response);
+storeValueFromQuery ("total_users", $users_query, $response);
+
+echo json_encode($response);
