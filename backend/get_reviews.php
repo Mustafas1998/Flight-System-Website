351eb30911fcc3b9f382490724a5e116e16407a2
AliAbdelAall <3laych@gmail.com>
2024-03-19 21:33:55 +0200

    added get-flights.php file

--- backend/get_reviews.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("connection.php");
-
-$query = mysqli -> prepare(
-"select u.username, f.flight_id, r.review_text, r.rating(select)
-from reviews r
-join flights f on f.flight_id = r.flight_id 
-join users u on u.user_id = r.user_id"
-);
