7467f80026813b552eb042586d3b918efc8d7968
AliAbdelAall <3laych@gmail.com>
2024-03-20 05:49:14 +0200

    added get-flights.php file

--- backend/get-flights.php
+++ backend/get-flights.php
@@ -1,33 +1,42 @@
 <?php
 include("connection.php");
 
-$destination = $_POST['destination'];
-$country = $_POST['country'];
-$price = $_POST['price'];
-$departure_date = $_POST['departure_date'];
-$arrival_date = $_POST['arrival_date'];
-$flight_status = $_POST['flight_status'];
-$airline_id = $_POST['airline_id'];
-$departure_airport_id = $_POST['departure_airport_id'];
-$arrival_airport_id = $_POST['arrival_airport_id'];
-$airplane_id = $_POST['airplane_id'];
+$flight_id =isset($_POST['flight_id']) ? $_POST['flight_id'] : false;
 
-$sql = "INSERT INTO flights (destination, country, price, departure_date, arrival_date, flight_status, airline_id, departure_airport_id, arrival_airport_id, airplane_id) 
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-$results = $mysqli->prepare($sql);
+if($flight_id){
+	$query = $mysqli->prepare("SELECT * FROM flights");
 
-
-
-$results->bind_param("ssisssiiii",$destination, $country, $price, $departure_date, $arrival_date, $flight_status, $airline_id, $departure_airport_id, $arrival_airport_id, $airplane_id);
-$results->execute();
-
-if ($results->execute()) {
-	$response['status']='success';
-	$response['message']='Flight Added successfully';
-
-} else {
-	$response['status']='failed';
-    $response['message']='failed to create flight';
+}else{
+	$query = $mysqli->prepare("SELECT * FROM flights WHERE flight_id = ?");
+	$query -> bind_param("i", $flight_id);
 }
+$query->execute();
+$query->store_result();
+$num_rows = $query->num_rows();
 
+if ($num_rows === 0) {
+    $response["status"] = "empty";
+  
+}else{
+	$array_list = [];
+	$query->bind_result($flight_id,$destintion,$country,$price,$departure_date,$arrival_date,$flight_status,$airline_id,$departure_airport_id,$arrival_airport_id,$airplane_id);
+	while ($query->fetch()){
+			$array = [
+					"flight_id"=> $flight_id,
+					"destination"=> $destintion,
+					"country"=> $country,
+					"price"=> $price,
+					"departure_date"=> $departure_date,
+					"arrival_date" => $arrival_date,
+					"flight_status"=> $flight_status,
+					"airline_id"=> $airline_id,
+					"departure_airport_id"=> $departure_airport_id,
+					"arrival_airport_id"=> $arrival_airport_id,
+					"airplane_id"=> $airplane_id,  
+			];
+			$array_list[] = $array;	
+	}
+	$response["status"] = "success";
+	$response["flights"] = $array_list;
+}
 echo json_encode($response);
